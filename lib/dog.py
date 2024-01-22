924aa9c004eff433e737178b1bf2258ea05bfdd7
Joseph <joseph.dena@student.moringaschool.com>
2024-01-22 21:51:00 +0300

    complete assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,59 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+
+# Creating instances of the Dog class
+fido = Dog()
+snoopy = Dog()
+lassie = Dog()
+
+# Calling the bark method for each dog
+fido.bark()   # Output: Woof!
+snoopy.bark() # Output: Woof!
+lassie.bark() # Output: Woof!
+
+# Calling the sit method for each dog
+# The sit method will print "The dog is sitting" for each instance
+fido.sit()    # Output: The dog is sitting
+snoopy.sit()  # Output: The dog is sitting
+lassie.sit()  # Output: The dog is sitting
+
+
+class Person:
+    
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+# Creating instances of the Person class
+#guy = Person()
+
+# Calling the talk method for each person
+#guy.talk()    # Output: Hello World!
+
+# Calling the walk method for each person
+#guy.walk()    # Output: The person is walking.
+
+
+    
+               
+               
+               
+               
+               
+    
+
+    
+    
+    
+    
+    
+    
